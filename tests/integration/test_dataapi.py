182318b6d7bcaebb7bd16be89a2293f781de2a80
Mark Kelly <markkellyeire@gmail.com>
2022-10-20 12:51:57 +0100

    [TESTS] Added a test case to ensure special characters can be used during signature creation LRN-36469

--- tests/integration/test_dataapi.py
+++ tests/integration/test_dataapi.py
@@ -78,6 +78,25 @@ class IntegrationTestDataApiClient(unittest.TestCase):
         assert len(results) == 2
         assert results == {'item_2', 'item_3'}
 
+    def test_real_request_with_special_characters(self):
+        """Make a request against Data Api to ensure the SDK works"""
+        client = DataApi()
+
+        # Add a reference containing special characters to ensure
+        # signature creation works with special characters in the request
+        local_items_request = items_request.copy()  # prevent modifying the base fixture
+        local_items_request['references'] = items_request['references'].copy()  # prevent modifying the base fixture's list
+        local_items_request['references'].append('тест')
+
+        res = client.request(self.__build_base_url() + items_endpoint, security, consumer_secret, items_request,
+                             action)
+        returned_json = res.json()
+
+        assert len(returned_json['data']) > 0
+
+        returned_ref = returned_json['data'][0]['reference']
+        assert returned_ref in items_request['references']
+
     def test_real_question_request(self):
         """Make a request against Data Api to ensure the SDK works"""
         client = DataApi()
