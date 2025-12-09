53013dee8bf3bf19969d294cc259190e5c6a27d5
Jack O'Connor <jack.oconnor@learnosity.com>
2025-12-09 17:11:44 +0000

    Fetch questions + responses from Data API in `get_report_data`

--- sandbox/utils/lrn_api_utils.py
+++ sandbox/utils/lrn_api_utils.py
@@ -1,5 +1,33 @@
+from docs.quickstart import config
+from learnosity_sdk.request import DataApi
+
+itembank_uri = "https://data.learnosity.com/latest-lts/sessions/responses"
+
+security_packet = {
+    "consumer_key": config.consumer_key,
+    "domain": "localhost",
+}
+
+
 def get_report_data(user_id, session_id):
-    with open('sandbox/question_answer_dump.json', 'r') as f:
+    # Get response_ids from Data API
+    data_api = DataApi()
+    data_request = {
+        "user_id": [user_id],
+        "session_id": [session_id],
+        "activity_id": ["quickstart_examples_activity_001"],
+        "limit": 10,
+    }
+    result = data_api.request(
+        itembank_uri, security_packet, config.consumer_secret, data_request, "get"
+    )
+    response = result.json()
+    # `response`` here contains the questions and responses for an activity.
+    # The format is slightly different to the private network requests in the
+    # network requests against the questionresponses API we were looking at.
+
+    with open("sandbox/question_answer_dump.json", "r") as f:
         report_data = f.read()
 
     return report_data
+
