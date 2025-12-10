798d424c8c13a96af73964b38bc7f24132cfbc7e
Jack O'Connor <jack.oconnor@learnosity.com>
2025-12-10 12:19:49 +0000

    Retrieve activity report data using Data APIs

--- sandbox/utils/lrn_api_utils.py
+++ sandbox/utils/lrn_api_utils.py
@@ -1,7 +1,8 @@
 from docs.quickstart import config
 from learnosity_sdk.request import DataApi
 
-itembank_uri = "https://data.learnosity.com/latest-lts/sessions/responses"
+responses_uri = "https://data.learnosity.com/latest-lts/sessions/responses"
+questions_uri = "https://data.learnosity.com/latest-lts/itembank/questions"
 
 security_packet = {
     "consumer_key": config.consumer_key,
@@ -10,24 +11,48 @@ security_packet = {
 
 
 def get_report_data(user_id, session_id):
-    # Get response_ids from Data API
+    # Get responses from Data API
     data_api = DataApi()
-    data_request = {
+    responses_request = {
         "user_id": [user_id],
         "session_id": [session_id],
         "activity_id": ["quickstart_examples_activity_001"],
         "limit": 10,
     }
-    result = data_api.request(
-        itembank_uri, security_packet, config.consumer_secret, data_request, "get"
-    )
-    response = result.json()
-    # `response`` here contains the questions and responses for an activity.
-    # The format is slightly different to the private network requests in the
-    # network requests against the questionresponses API we were looking at.
+    responses_result = data_api.request(
+        responses_uri, security_packet, config.consumer_secret, responses_request, "get"
+    ).json()
 
-    with open("sandbox/question_answer_dump.json", "r") as f:
-        report_data = f.read()
+    # Get questions from Data API
+    question_references = [
+        response["question_reference"]
+        for response in responses_result["data"][0]["responses"]
+    ]
+    questions_request = {"references": question_references}
+    question_result = data_api.request(
+        questions_uri, security_packet, config.consumer_secret, questions_request, "get"
+    ).json()
+
+    # Index the responses and questions by question reference
+    responses_by_question_reference = dict()
+    for response in responses_result["data"][0]["responses"]:
+        question_reference = response["question_reference"]
+        responses_by_question_reference[question_reference] = response
+
+    questions_by_question_reference = dict()
+    for question in question_result["data"]:
+        question_reference = question["reference"]
+        questions_by_question_reference[question_reference] = question["data"]
+
+    # Build up report_data for LLM
+    report_data = []
+    for idx, question_reference in enumerate(responses_by_question_reference.keys()):
+        response = responses_by_question_reference[question_reference]
+        question = questions_by_question_reference[question_reference]
+        report_data_entry = {"id": idx}
+
+        report_data_entry["question"] = question
+        report_data_entry["response"] = {"value": response["response"]["value"]}
+        report_data.append(report_data_entry)
 
     return report_data
-
